f8cee0f354e1bf1c0c03f23d5a9c8f39ac0557e9
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2015-11-06 07:52:47 +0000

    Added connectivity argument to remove_small_objects and made Flake8 compliant.

--- jicbioimage/transform/__init__.py
+++ jicbioimage/transform/__init__.py
@@ -5,8 +5,6 @@ purpose transformations that have had the
 :func:`jicbioimage.core.transformation` function decorator applied to them.
 """
 
-from functools import wraps
-
 import numpy as np
 
 import scipy.ndimage.filters
@@ -15,8 +13,6 @@ import skimage.io
 import skimage.morphology
 import skimage.exposure
 
-from jicbioimage.core.io import AutoName, AutoWrite
-from jicbioimage.core.image import Image
 from jicbioimage.core.util.array import (
     normalise,
     reduce_stack,
@@ -32,20 +28,22 @@ __version__ = "0.4.6"
 @transformation
 def max_intensity_projection(stack):
     """Return maximum intensity projection of a stack.
-    
-    :param stack: 3D array from which to project third dimension 
+
+    :param stack: 3D array from which to project third dimension
     :returns: :class:`jicbioimage.core.image.Image`
     """
     return reduce_stack(stack, max)
 
+
 @transformation
 def min_intensity_projection(stack):
     """Return minimum intensity projection of a stack.
-    
-    :param stack: 3D array from which to project third dimension 
+
+    :param stack: 3D array from which to project third dimension
     :returns: :class:`jicbioimage.core.image.Image`
     """
     return reduce_stack(stack, min)
+
 
 @transformation
 @dtype_contract(input_dtype=np.float, output_dtype=np.float)
@@ -56,33 +54,40 @@ def smooth_gaussian(image, sigma=1):
     :param sigma: standard deviation
     :returns: :class:`jicbioimage.core.image.Image`
     """
-    return scipy.ndimage.filters.gaussian_filter(image, sigma=sigma, mode="nearest")
+    return scipy.ndimage.filters.gaussian_filter(image,
+                                                 sigma=sigma,
+                                                 mode="nearest")
+
 
 @transformation
 @dtype_contract(output_dtype=np.float)
 def equalize_adaptive_clahe(image, ntiles=8, clip_limit=0.01):
     """Return contrast limited adaptive histogram equalized image.
-    
+
     The return value is normalised to the range 0 to 1.
 
-    :param image: numpy array or :class:`jicbioimage.core.image.Image` of dtype float
+    :param image: numpy array or :class:`jicbioimage.core.image.Image`
+                  of dtype float
     :param ntiles: number of tile regions
     :param clip_limit: clipping limit in range 0 to 1,
                        higher values give more contrast
     """
     # Convert input for skimage.
     skimage_float_im = normalise(image)
-    
+
     if np.all(skimage_float_im):
         raise(RuntimeError("Cannot equalise when there is no variation."))
-    
+
     normalised = skimage.exposure.equalize_adapthist(skimage_float_im,
-        ntiles_x=ntiles, ntiles_y=ntiles, clip_limit=clip_limit)
+                                                     ntiles_x=ntiles,
+                                                     ntiles_y=ntiles,
+                                                     clip_limit=clip_limit)
 
     assert np.max(normalised) == 1.0
     assert np.min(normalised) == 0.0
 
     return normalised
+
 
 @transformation
 @dtype_contract(output_dtype=np.bool)
@@ -92,12 +97,15 @@ def threshold_otsu(image, multiplier=1.0):
     otsu_value = skimage.filters.threshold_otsu(image)
     return image > otsu_value * multiplier
 
+
 @transformation
 @dtype_contract(input_dtype=np.bool, output_dtype=np.bool)
-def remove_small_objects(image, min_size=50):
+def remove_small_objects(image, min_size=50, connectivity=1):
     """Remove small objects from an boolean image.
 
     :param image: boolean numpy array or :class:`jicbioimage.core.image.Image`
     :returns: boolean :class:`jicbioimage.core.image.Image`
-    """ 
-    return skimage.morphology.remove_small_objects(image, min_size=min_size)
+    """
+    return skimage.morphology.remove_small_objects(image,
+                                                   min_size=min_size,
+                                                   connectivity=connectivity)
--- tests/transform_functional_tests.py
+++ tests/transform_functional_tests.py
@@ -10,6 +10,7 @@ HERE = os.path.dirname(__file__)
 DATA_DIR = os.path.join(HERE, 'data')
 TMP_DIR = os.path.join(HERE, 'tmp')
 
+
 class GeneralPurposeTransoformTests(unittest.TestCase):
 
     def setUp(self):
@@ -28,56 +29,56 @@ class GeneralPurposeTransoformTests(unittest.TestCase):
         from jicbioimage.transform import max_intensity_projection
         from jicbioimage.core.image import Image
         slice0 = np.array(
-            [[ 0, 1, 2],
-             [ 0, 1, 2],
-             [ 0, 1, 2]], dtype=np.uint8)
+            [[0, 1, 2],
+             [0, 1, 2],
+             [0, 1, 2]], dtype=np.uint8)
         slice1 = np.array(
-            [[ 2, 1, 0],
-             [ 2, 1, 0],
-             [ 2, 1, 0]], dtype=np.uint8)
+            [[2, 1, 0],
+             [2, 1, 0],
+             [2, 1, 0]], dtype=np.uint8)
         expected = np.array(
-            [[ 2, 1, 2],
-             [ 2, 1, 2],
-             [ 2, 1, 2]], dtype=np.uint8)
+            [[2, 1, 2],
+             [2, 1, 2],
+             [2, 1, 2]], dtype=np.uint8)
         stack = np.dstack([slice0, slice1])
         max_projection = max_intensity_projection(stack)
-        self.assertTrue( np.array_equal(expected, max_projection) )
-        self.assertTrue( isinstance(max_projection, Image) )
-        
+        self.assertTrue(np.array_equal(expected, max_projection))
+        self.assertTrue(isinstance(max_projection, Image))
+
     def test_min_intensity_projection(self):
         from jicbioimage.transform import min_intensity_projection
         from jicbioimage.core.image import Image
         slice0 = np.array(
-            [[ 0, 1, 2],
-             [ 0, 1, 2],
-             [ 0, 1, 2]], dtype=np.uint8)
+            [[0, 1, 2],
+             [0, 1, 2],
+             [0, 1, 2]], dtype=np.uint8)
         slice1 = np.array(
-            [[ 2, 1, 0],
-             [ 2, 1, 0],
-             [ 2, 1, 0]], dtype=np.uint8)
+            [[2, 1, 0],
+             [2, 1, 0],
+             [2, 1, 0]], dtype=np.uint8)
         expected = np.array(
-            [[ 0, 1, 0],
-             [ 0, 1, 0],
-             [ 0, 1, 0]], dtype=np.uint8)
+            [[0, 1, 0],
+             [0, 1, 0],
+             [0, 1, 0]], dtype=np.uint8)
         stack = np.dstack([slice0, slice1])
         min_projection = min_intensity_projection(stack)
-        self.assertTrue( np.array_equal(expected, min_projection) )
-        self.assertTrue( isinstance(min_projection, Image) )
-        
+        self.assertTrue(np.array_equal(expected, min_projection))
+        self.assertTrue(isinstance(min_projection, Image))
+
     def test_smooth_gaussian(self):
         from jicbioimage.transform import smooth_gaussian
         from jicbioimage.core.image import Image
         array = np.array(
-            [[ 0.,  0.,  0.],
-             [ 0.,  1.,  0.],
-             [ 0.,  0.,  0.]], dtype=np.float)
+            [[0., 0., 0.],
+             [0., 1., 0.],
+             [0., 0., 0.]], dtype=np.float)
         expected = np.array(
-            [[ 0.05855018,  0.09653293,  0.05855018],
-             [ 0.09653293,  0.15915589,  0.09653293],
-             [ 0.05855018,  0.09653293,  0.05855018]], dtype=np.float)
+            [[0.05855018, 0.09653293, 0.05855018],
+             [0.09653293, 0.15915589, 0.09653293],
+             [0.05855018, 0.09653293, 0.05855018]], dtype=np.float)
         smoothed = smooth_gaussian(array)
-        self.assertTrue( np.allclose(expected, smoothed) )
-        self.assertTrue( isinstance(smoothed, Image) )
+        self.assertTrue(np.allclose(expected, smoothed))
+        self.assertTrue(isinstance(smoothed, Image))
 
         # The smooth_gaussian function only makes sense on dtype np.float.
         with self.assertRaises(TypeError):
@@ -87,26 +88,26 @@ class GeneralPurposeTransoformTests(unittest.TestCase):
         from jicbioimage.transform import equalize_adaptive_clahe
         from jicbioimage.core.image import Image
         array = np.array(
-            [[ 2., 2., 1., 1., 4., 4.],
-             [ 2., 1., 1., 1., 1., 4.],
-             [ 1., 1., 1., 1., 1., 1.],
-             [ 1., 1., 1., 1., 1., 1.],
-             [ 6., 1., 1., 1., 1., 8.],
-             [ 6., 6., 1., 1., 8., 8.]], dtype=np.uint8)
+            [[2., 2., 1., 1., 4., 4.],
+             [2., 1., 1., 1., 1., 4.],
+             [1., 1., 1., 1., 1., 1.],
+             [1., 1., 1., 1., 1., 1.],
+             [6., 1., 1., 1., 1., 8.],
+             [6., 6., 1., 1., 8., 8.]], dtype=np.uint8)
         expected = np.array(
-            [[ 1., 1., 0., 0., 1., 1.],
-             [ 1., 0., 0., 0., 0., 1.],
-             [ 0., 0., 0., 0., 0., 0.],
-             [ 0., 0., 0., 0., 0., 0.],
-             [ 1., 0., 0., 0., 0., 1.],
-             [ 1., 1., 0., 0., 1., 1.]], dtype=np.float)
+            [[1., 1., 0., 0., 1., 1.],
+             [1., 0., 0., 0., 0., 1.],
+             [0., 0., 0., 0., 0., 0.],
+             [0., 0., 0., 0., 0., 0.],
+             [1., 0., 0., 0., 0., 1.],
+             [1., 1., 0., 0., 1., 1.]], dtype=np.float)
         equalised = equalize_adaptive_clahe(array, ntiles=2)
-        self.assertTrue( np.array_equal(expected, equalised) )
-        self.assertTrue( isinstance(equalised, Image) )
+        self.assertTrue(np.array_equal(expected, equalised))
+        self.assertTrue(isinstance(equalised, Image))
 
         # Cannot equalise an image with no variation.
         with self.assertRaises(RuntimeError):
-            array = np.ones((6,6))
+            array = np.ones((6, 6))
             equalised = equalize_adaptive_clahe(array, ntiles=2)
 
     def test_threshold_otsu(self):
@@ -115,56 +116,65 @@ class GeneralPurposeTransoformTests(unittest.TestCase):
 
         # Test with uint8.
         array = np.array(
-            [[ 1,  2,  3],
-             [ 7,  8,  9]], dtype=np.uint8)
+            [[1,  2,  3],
+             [7,  8,  9]], dtype=np.uint8)
         expected = np.array(
-            [[ 0,  0,  0],
-             [ 1,  1,  1]], dtype=np.bool)
+            [[0,  0,  0],
+             [1,  1,  1]], dtype=np.bool)
         thresholded = threshold_otsu(array)
-        self.assertTrue( np.array_equal(expected, thresholded) )
-        self.assertTrue( isinstance(thresholded, Image) )
+        self.assertTrue(np.array_equal(expected, thresholded))
+        self.assertTrue(isinstance(thresholded, Image))
 
         # Test with float.
         array = np.array(
-            [[ 1,  2,  3],
-             [ 7,  8,  9]], dtype=np.float)
+            [[1,  2,  3],
+             [7,  8,  9]], dtype=np.float)
         expected = np.array(
-            [[ 0,  0,  0],
-             [ 1,  1,  1]], dtype=np.bool)
+            [[0,  0,  0],
+             [1,  1,  1]], dtype=np.bool)
         thresholded = threshold_otsu(array)
-        self.assertTrue( np.array_equal(expected, thresholded) )
-        self.assertTrue( isinstance(thresholded, Image) )
+        self.assertTrue(np.array_equal(expected, thresholded))
+        self.assertTrue(isinstance(thresholded, Image))
 
     def test_threshold_otsu_multiplier(self):
         from jicbioimage.transform import threshold_otsu
         array = np.array(
-            [[ 1,  2,  3],
-             [ 7,  8,  9]], dtype=np.uint8)
+            [[1,  2,  3],
+             [7,  8,  9]], dtype=np.uint8)
         # Threshold used: 3 * 0.6 = 1.79
         expected = np.array(
-            [[ 0,  1,  1],
-             [ 1,  1,  1]], dtype=np.bool)
+            [[0,  1,  1],
+             [1,  1,  1]], dtype=np.bool)
         thresholded = threshold_otsu(array, multiplier=0.6)
-        self.assertTrue( np.array_equal(expected, thresholded) )
+        self.assertTrue(np.array_equal(expected, thresholded))
 
     def test_remove_small_objects(self):
         from jicbioimage.transform import remove_small_objects
         from jicbioimage.core.image import Image
         array = np.array(
-            [[ 0,  0,  0, 0, 1],
-             [ 0,  1,  1, 0, 0],
-             [ 0,  1,  1, 0, 0],
-             [ 0,  0,  0, 0, 1],
-             [ 1,  1,  0, 1, 1]], dtype=np.bool)
-        expected = np.array(
-            [[ 0,  0,  0, 0, 0],
-             [ 0,  1,  1, 0, 0],
-             [ 0,  1,  1, 0, 0],
-             [ 0,  0,  0, 0, 0],
-             [ 0,  0,  0, 0, 0]], dtype=np.bool)
+            [[0,  0,  0, 0, 1],
+             [0,  1,  1, 0, 0],
+             [0,  1,  1, 0, 0],
+             [0,  0,  0, 1, 0],
+             [1,  1,  0, 1, 0]], dtype=np.bool)
+        expected_con1 = np.array(
+            [[0,  0,  0, 0, 0],
+             [0,  1,  1, 0, 0],
+             [0,  1,  1, 0, 0],
+             [0,  0,  0, 0, 0],
+             [0,  0,  0, 0, 0]], dtype=np.bool)
         no_small = remove_small_objects(array, min_size=4)
-        self.assertTrue( np.array_equal(expected, no_small) )
-        self.assertTrue( isinstance(no_small, Image) )
+        self.assertTrue(np.array_equal(expected_con1, no_small))
+        self.assertTrue(isinstance(no_small, Image))
+
+        expected_con2 = np.array(
+            [[0,  0,  0, 0, 0],
+             [0,  1,  1, 0, 0],
+             [0,  1,  1, 0, 0],
+             [0,  0,  0, 1, 0],
+             [0,  0,  0, 1, 0]], dtype=np.bool)
+        no_small = remove_small_objects(array, min_size=4, connectivity=2)
+        self.assertTrue(np.array_equal(expected_con2, no_small))
 
         # The smooth_gaussian function only makes sense on dtype np.bool.
         with self.assertRaises(TypeError):
